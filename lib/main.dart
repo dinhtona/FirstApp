e216cfc9dbd3b8f227502632e30e88ad1467e42b
dinhtona <32251215+dinhtona@users.noreply.github.com>
2021-01-11 12:41:34 +0700

    Cách để cho bottom app bar khuyết xuống và bo tròn
    bottomNavigationBar: BottomAppBar(
    //somthing
              shape: CircularNotchedRectangle(),
              clipBehavior: Clip.antiAliasWithSaveLayer,
    )

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -44,6 +45,100 @@ class MyApp extends StatelessWidget {
               },
             )
           ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          // currentIndex: 0,
+          // items: [
+          //   BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'HH'),
+          //   BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'HH'),
+          //   BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'HH'),
+          //   // BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'HH')
+          // ],
+          shape: CircularNotchedRectangle(),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          color: Colors.blue,
+          elevation: 0,
+
+          child: Container(
+            // color: Colors.red,
+            height: 60,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.home,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.local_activity,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      //Icon(Icons.escalator),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.games_rounded,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.escalator,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.pinkAccent[200],
+          child: Icon(
+            Icons.qr_code,
+          ),
+          // shape: CircularNotchedRectangle(),
+
+          onPressed: () => print('Add clicked'),
+          elevation: 0,
+          hoverElevation: 20,
         ),
       ),
     );
