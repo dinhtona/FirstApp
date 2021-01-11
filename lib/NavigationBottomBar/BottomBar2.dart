116f78e9043ab1e024ef2913bd3947bbbf72681e
dinhtona <32251215+dinhtona@users.noreply.github.com>
2021-01-11 13:31:22 +0700

    StatefulWidget Xample state

--- lib/NavigationBottomBar/BottomBar2.dart
+++ lib/NavigationBottomBar/BottomBar2.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
-class BottomBar2 extends StatelessWidget {
+class BottomBar2 extends StatefulWidget {
+  @override
+  _BottomBar2State createState() => _BottomBar2State();
+}
+
+class _BottomBar2State extends State<BottomBar2> {
+  int _currentindex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _currentindex = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //Set color for Bottom Bar
@@ -16,18 +31,25 @@ class BottomBar2 extends StatelessWidget {
         clipBehavior: Clip.antiAliasWithSaveLayer, //Bo góc nav
         // notchMargin: 7,
         child: BottomNavigationBar(
+          currentIndex: _currentindex,
           // elevation: 5.0,
           type: BottomNavigationBarType.fixed, //Căn đều bottom bar item
           // backgroundColor: Colors.blueAccent,
-          selectedItemColor: Colors.blueAccent,
-          unselectedItemColor: Colors.red,
+          // selectedItemColor: Colors.blueAccent,
+          // unselectedItemColor: Colors.red,
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.home_work), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.home_work), label: 'Home'),
-            // BottomNavigationBarItem(icon: Icon(Icons.home_work), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.home_work), label: ''),
             BottomNavigationBarItem(icon: Icon(Icons.home_work), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.home_work), label: 'Home'),
           ],
+          onTap: (index) {
+            print(index);
+            setState(() {
+              _currentindex = index;
+            });
+          },
         ),
       ),
     );
