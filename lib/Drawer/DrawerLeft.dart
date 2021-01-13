c5767f56e7c41dbb973d5e0facb712fc953d4ae1
dinhtona <32251215+dinhtona@users.noreply.github.com>
2021-01-13 20:00:06 +0700

    Fixed Header Drawer, Before Add Account

--- lib/Drawer/DrawerLeft.dart
+++ lib/Drawer/DrawerLeft.dart
@@ -9,53 +9,70 @@ class DrawerLeft extends StatelessWidget {
 
     return SafeArea(
       child: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            _createHeader(context),
-            _createDrawerItem(icon: Icons.stars, text: 'Useful Links'),
-            ExpansionTile(
-                title: Text('Làm đẹp'),
-                childrenPadding: EdgeInsets.only(left: 20),
-                //tilePadding: EdgeInsets.only(left: 10),
-                initiallyExpanded: true,
-                leading: Icon(Icons.favorite),
-                children: <Widget>[
-                  _createDrawerItem(
-                    icon: Icons.contacts,
-                    text: 'Contacts',
-                  ),
-                  _createDrawerItem(
-                    icon: Icons.event,
-                    text: 'Events',
-                  ),
-                  _createDrawerItem(
-                    icon: Icons.note,
-                    text: 'Notes',
-                  ),
-                ]),
-            ExpansionTile(
-                title: Text('Vui chơi'),
-                childrenPadding: EdgeInsets.only(left: 20),
-                //tilePadding: EdgeInsets.only(left: 10),
-                initiallyExpanded: true,
-                leading: Icon(Icons.airline_seat_recline_extra),
-                children: <Widget>[
-                  _createDrawerItem(
-                      icon: Icons.collections_bookmark, text: 'Steps'),
-                  _createDrawerItem(icon: Icons.face, text: 'Authors'),
-                ]),
-            _createDrawerItem(
-                icon: Icons.account_box, text: 'Flutter Documentation'),
-            _createDrawerItem(icon: Icons.bug_report, text: 'Report an issue'),
-            ListTile(
-              title: Text('0.0.1'),
-              onTap: () {},
-            ),
-            Divider(),
-          ],
-        ),
-      ),
+          elevation: 20.0,
+          // child: ListView(
+          // padding: EdgeInsets.zero,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(
+                height: 180,
+                child: _createHeader(context),
+              ),
+              Expanded(
+                child: ListView(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  children: [
+                    _createDrawerItem(icon: Icons.stars, text: 'Useful Links'),
+                    ExpansionTile(
+                        title: Text('Làm đẹp'),
+                        childrenPadding: EdgeInsets.only(left: 20),
+                        //tilePadding: EdgeInsets.only(left: 10),
+                        initiallyExpanded: true,
+                        leading: Icon(Icons.favorite),
+                        children: <Widget>[
+                          _createDrawerItem(
+                            icon: Icons.contacts,
+                            text: 'Contacts',
+                          ),
+                          _createDrawerItem(
+                            icon: Icons.event,
+                            text: 'Events',
+                          ),
+                          _createDrawerItem(
+                              icon: Icons.note,
+                              text: 'Notes',
+                              onTap: () {
+                                print('Notes');
+                              }),
+                        ]),
+                    ExpansionTile(
+                        title: Text('Vui chơi'),
+                        childrenPadding: EdgeInsets.only(left: 20),
+                        //tilePadding: EdgeInsets.only(left: 10),
+                        initiallyExpanded: true,
+                        leading: Icon(Icons.airline_seat_recline_extra),
+                        children: <Widget>[
+                          _createDrawerItem(
+                              icon: Icons.collections_bookmark, text: 'Steps'),
+                          _createDrawerItem(icon: Icons.face, text: 'Authors'),
+                        ]),
+                    _createDrawerItem(
+                        icon: Icons.account_box, text: 'Flutter Documentation'),
+                    _createDrawerItem(
+                        icon: Icons.bug_report, text: 'Report an issue'),
+                    ListTile(
+                      title: Text('0.0.1'),
+                      onTap: () {},
+                    ),
+                    Divider(),
+                  ],
+                ),
+              )
+            ],
+          )
+          // ),
+          ),
     );
     // Add a Drawer here in the next step.
   }
@@ -106,6 +123,7 @@ class DrawerLeft extends StatelessWidget {
     Color gradientEnd = Colors.black.withOpacity(0.9);
 
     return DrawerHeader(
+      curve: Curves.fastOutSlowIn,
       margin: EdgeInsets.zero,
       padding: EdgeInsets.zero,
       child: Stack(children: <Widget>[
@@ -144,7 +162,68 @@ class DrawerLeft extends StatelessWidget {
                 fontSize: 20.0,
                 fontWeight: FontWeight.w500),
           ),
-        )
+        ),
+        UserAccountsDrawerHeader(
+          // margin: EdgeInsets.only(bottom: 0),
+          accountName: Text(
+            "SON GO KU",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          accountEmail: Text("songoku@gmail.com"),
+          // arrowColor: Theme.of(context).primaryColorDark,
+          // decoration: BoxDecoration(
+          //   gradient: LinearGradient(colors: [
+          //     Theme.of(context).primaryColorDark,
+          //     Theme.of(context).primaryColor
+          //   ]),
+          //   // image: DecorationImage(
+          //   //     fit: BoxFit.fill,
+          //   //     image: NetworkImage(
+          //   //         "https://c4.wallpaperflare.com/wallpaper/379/942/360/dragon-ball-dragon-ball-super-wallpaper-preview.jpg")),
+          // ),
+          otherAccountsPictures: [
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(2.0),
+                child: CircleAvatar(
+                  radius: 38,
+                  backgroundImage: NetworkImage(
+                      "https://avatarfiles.alphacoders.com/558/55871.png"),
+                ),
+              ),
+            ),
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(2.0),
+                child: CircleAvatar(
+                  radius: 38,
+                  backgroundImage: NetworkImage(
+                      "https://i1.sndcdn.com/avatars-000083247059-ocdcqj-t500x500.jpg"),
+                ),
+              ),
+            ),
+          ],
+          currentAccountPicture: CircleAvatar(
+            radius: 100,
+            backgroundColor: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                  "https://i1.sndcdn.com/avatars-000272285088-bbpvne-t500x500.jpg",
+                ),
+              ),
+            ),
+          ),
+        ),
       ]),
     );
   }
