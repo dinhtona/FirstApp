a6501fdbd14818ccd2e1e2d49053a18fefdd9939
dinhtona <32251215+dinhtona@users.noreply.github.com>
2021-01-16 13:13:34 +0700

    AnimatedContainer
    https://www.youtube.com/watch?v=yI-8QHpGIP4&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=5
    
    https://api.flutter.dev/flutter/widgets/AnimatedContainer-class.html

--- lib/pages/practice_screen.dart
+++ lib/pages/practice_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Practice extends StatelessWidget {
-  Practice({this.title});
+class Practice extends StatefulWidget {
+  Practice({Key key}) : super(key: key);
+  @override
+  _PracticeState createState() => _PracticeState();
+}
 
-  final Widget title;
+class _PracticeState extends State<Practice> {
+  bool selected = false;
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Hello new page'));
+    return GestureDetector(
+      //Lắng nghe cử chỉ(onTap, onDoubleTap,...)
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+          child: AnimatedContainer(
+        //Hoạt ảnh các đối tượng
+        //
+        width: selected ? 200 : 100,
+        height: !selected ? 200 : 100,
+        color: selected ? Colors.greenAccent : Colors.yellow,
+        alignment: selected
+            ? Alignment.center
+            : AlignmentDirectional
+                .topCenter, //Chỉ có tác dụng đối với các thực thể bên trong (child)
+        duration: Duration(milliseconds: 1000), //Tốc độ chuyển động
+        // curve: Curves.easeInBack, //Mô tả chuyển động
+        child: Icon(Icons.access_alarms),
+        // child: FlutterLogo(
+        //   size: 75,
+        //   duration: Duration(milliseconds: 600), //Không tác dụng
+        //   textColor: Colors.red, //Không tác dụng
+        // ),
+      )),
+    );
   }
 }
