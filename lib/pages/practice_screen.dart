218ae9389cf036f84d99028a26ebb2aec7fcf38f
dinhtona <32251215+dinhtona@users.noreply.github.com>
2021-01-18 16:07:23 +0700

    SliverAppBar: Appbar vuốt vuốt kéo kéo :alien:

--- lib/pages/practice_screen.dart
+++ lib/pages/practice_screen.dart
@@ -1,51 +1,34 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
-import 'package:flutter_blurhash/flutter_blurhash.dart';
 
 class Practice extends StatelessWidget {
-  List<Widget> _getColumn() {
-    List<Widget> wg = [];
-
-    for (int i = 0; i <= 3; i++) {
-      wg.add(Container(
-        height: 100,
-        width: 100,
-        color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-            .withOpacity(0.7),
-      ));
-    }
-    return wg;
-  }
-
-  List<TableRow> _generateTableRow(int numRow) {
-    List<TableRow> wg = [];
-    for (int i = 0; i <= numRow; i++) {
-      wg.add(TableRow(children: _getColumn()));
-    }
-    return wg;
-  }
-
   @override
   Widget build(BuildContext context) {
-    // Random random = new Random();
-
     return Center(
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Table(
-          // columnWidths: {1, FractionColumnWidth(.2)},//Éo xài dc
-          border: TableBorder.all(),
-          defaultVerticalAlignment: TableCellVerticalAlignment.top,
-          defaultColumnWidth: FlexColumnWidth(2.0),
-
-          // IntrinsicColumnWidth(),
-          //FixedColumnWidth(70.0),
-          //FractionColumnWidth(0.5),
-          // FlexColumnWidth(2.0),
-
-          children: _generateTableRow(50),
-        ),
+      heightFactor: 500,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+              backgroundColor: Colors.grey,
+              expandedHeight: 150.0,
+              flexibleSpace: const FlexibleSpaceBar(
+                title: Text('Cậu vàng trên cỏ xanh'),
+                background: Positioned.fill(
+                  // top: 0,
+                  child: Image(
+                    image: AssetImage('assets/images/drawer_bg.png'),
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.add_circle),
+                  tooltip: 'Add new entry',
+                  onPressed: () {/* ... */},
+                ),
+              ])
+        ],
       ),
     );
   }
